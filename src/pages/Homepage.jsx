3173c09a28c1a2a784e1c00567c204cab1f66f64
Michela <micheladellagatta1@gmail.com>
2025-12-11 13:37:20 +0100

    Make AJAX call and load response into page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,40 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+const urlMovies = import.meta.env.VITE_ENDPOINT;
+
 export default function Homepage() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(urlMovies)
+      .then((response) => {
+        setMovies(response.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [urlMovies]);
+
   return (
     <>
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Welcome to Jurassic Park</h1>
-        <p className="text-lg">
-          Niente non so ancora che metterci nella homepage, preferisco la
-          back-end
-        </p>
-      </div>
+      {loading ? (
+        <div className="spinner-border text-primary" role="status"></div>
+      ) : (
+        <div className="container mt-4">
+          {movies.length > 0 ? (
+            <div className="row">
+              {movies.map((movie) => (
+                <li key={movie.id}>{movie.title}</li>
+              ))}
+            </div>
+          ) : (
+            <p className="text-center mt-4 text-white">
+              Oggi niente film, va a leggere un libro
+            </p>
+          )}
+        </div>
+      )}
     </>
   );
 }
