2fe1aaeab3fa6fae1b31399a045f29a125ae6255
Michela <micheladellagatta1@gmail.com>
2025-12-16 12:04:32 +0100

    Wrap app with provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Homepage from "./pages/Homepage";
 import Movie from "./pages/Movie";
 import NotFound from "./pages/NotFound";
 import AboutUs from "./pages/AboutUs";
+import { LoadingProvider } from "./context/LoadingContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/movie/:id" element={<Movie />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/notfound" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LoadingProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<Homepage />} />
+            <Route path="/movie/:id" element={<Movie />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/notfound" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </LoadingProvider>
   );
 }
 
